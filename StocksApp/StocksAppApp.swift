d941aa734860e6e923aa66e4c93b2051d5049b33
Jon Chang <jonasenchang@gmail.com>
2023-07-13 12:51:16 -0700

    Initial Commit

---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by Jon Chang on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
